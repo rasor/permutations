d1cf9771669350cfc1620aaa59f159908ec2e1e2
rasor <soren@rasor.dk>
2018-02-25 21:22:23 +0100

    Preparing for combinations without repts

---
+++ Combinations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Combinations
+{ 
+    /// <summary>
+    /// Combinations Without Repetion
+    /// https://math.stackexchange.com/questions/2449850/listing-every-single-possibility-of-combinations-without-repetition-of-20-elemen
+    /// https://www.hackmath.net/en/calculator/combinations-and-permutations?n=4&k=2&order=0&repeat=0
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
